9414853dc80012b029447fa3cb0ba09662446f2e
Joy Kent <joy@automonic.ai>
2018-11-16 11:10:40 -0800

    Add Dockerfile.template, trufflehawk and fix build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 ./mvnw versions:set -DnewVersion="0.13.1-incubating-SNAPSHOT-au-$FINAL_OR_BRANCH_SNAPSHOT_VERSION"
-./mvnw -DskipTests=true deploy
+./mvnw -DskipTests=true -Pdist install
+
+sed "s%<DRUID_VERSION>%0.13.1-incubating-SNAPSHOT-au-$FINAL_OR_BRANCH_SNAPSHOT_VERSION%g" Dockerfile.template > Dockerfile
