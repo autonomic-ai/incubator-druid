09760bd9253a7bdbcbe92f1d76c07ba4f506d37f
Joy Kent <joy@automonic.ai>
2018-11-16 11:10:40 -0800

    Add Dockerfile.template and fix build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 ./mvnw versions:set -DnewVersion="0.12.3-au-$FINAL_OR_BRANCH_SNAPSHOT_VERSION"
 ./mvnw -DskipTests=true install
+
+sed "s%<DRUID_VERSION>%0.12.3-au-$FINAL_OR_BRANCH_SNAPSHOT_VERSION%g" Dockerfile.template > Dockerfile
