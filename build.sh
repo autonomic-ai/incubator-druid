9b49b056046b72d03416c735430e2b5acd5bc7dd
Joy Kent <joy@automonic.ai>
2019-01-08 09:49:58 -0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-./mvnw versions:set -DnewVersion="0.13.1-incubating-SNAPSHOT-au-$FINAL_OR_BRANCH_SNAPSHOT_VERSION"
-./mvnw -DskipTests=true -Pdist install
+./mvnw versions:set -DnewVersion="au-0.13.0-incubating-$FINAL_OR_BRANCH_SNAPSHOT_VERSION"
+./mvnw -DskipTests=true -Pdist deploy
 
-sed "s%<DRUID_VERSION>%0.13.1-incubating-SNAPSHOT-au-$FINAL_OR_BRANCH_SNAPSHOT_VERSION%g" Dockerfile.template > Dockerfile
+sed "s%<DRUID_VERSION>%au-0.13.0-incubating-$FINAL_OR_BRANCH_SNAPSHOT_VERSION%g" Dockerfile.template > Dockerfile
