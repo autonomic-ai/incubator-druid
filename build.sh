c9e27976404e98e0f5b0acabef4d91488d7a3b69
jorbay-au <jorgeorbay@JorgeO-MBPT-PA.local>
2019-02-07 15:58:51 -0800

    Activate testing

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 ./mvnw versions:set -DnewVersion="$FINAL_OR_BRANCH_SNAPSHOT_VERSION"
-./mvnw -DskipTests=true -Pdist deploy
+./mvnw -Pdist deploy
 
 if [ -z $FINAL_OR_BRANCH_SNAPSHOT_VERSION ]; then
     sed "s%<DRUID_VERSION>%0.13.1-incubating-SNAPSHOT%g" Dockerfile.template > Dockerfile
